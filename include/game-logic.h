99de9290cddd5bce81e6121ca61d3dc24a799145
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-14 16:38:18 -0600

    Add interface for game-logic.c

---
+++ include/game-logic.h
@@ -0,0 +1,21 @@
+#ifndef GAME_LOGIC_H
+
+#define GAME_LOGIC_H
+
+typedef struct Game {
+    int **tablero; // Doble puntero para tener un array 2D
+    int puntuacion;
+    int tamanoTablero;
+} Game;
+
+int checkSize();
+
+int init_board(Game* game);
+
+void printTablero(Game* game);
+
+void addCasillaRandom(Game* game);
+
+void freeTablero(Game* game);
+
+#endif /* GAME_LOGIC_H */
