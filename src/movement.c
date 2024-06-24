c20c1cb0d9861a22f01b2988f5569dd02c3ae2dd
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-24 08:45:19 -0600

    Add fusionarCasillas function for upkey direction

--- src/movement.c
+++ src/movement.c
@@ -21,9 +21,28 @@ void moverCasillas(Game* game, char* direccion) {
             }
         }
     }
+
+    if (casillasMovidas) {
+        fusionarCasillas(game, direccion);
+        moverCasillas(game, direccion);
+    }
+
 }
 
 
-void fusionarCasillas(Game* game, char direccion) {
+void fusionarCasillas(Game* game, char* direccion) {
+    for (int i=0; i < game->tamanoTablero - 1; i++) {
 
+        for (int j=0; j < game->tamanoTablero; j++) {
+            
+            // No es una casilla vacia
+            if ((game->tablero[i][j] != 0) && (game->tablero[i][j] == game->tablero[i+1][j])) {
+                
+                game->tablero[i][j] *= 2;
+                game->tablero[i+1][j] = 0;
+
+                // Pendiente
+            }
+        }
+    }
 }
