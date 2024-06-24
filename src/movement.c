406f76bebf81f934704910e5a9f5cb92166fc3e9
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-24 10:13:35 -0600

    Add movement for all 4 directions

--- src/movement.c
+++ src/movement.c
@@ -1,36 +1,111 @@
 #include "../include/game-logic.h"
 #include "../include/movement.h"
+#include <stdio.h>
 
-void moverCasillas(Game* game, char* direccion) {
+void moverCasillas(Game* game, char direccion) {
     int casillasMovidas = 0;
 
-    for (int i=0; i < game->tamanoTablero; i++) {
 
-        for (int j=0; j < game->tamanoTablero; j++) {
-            
-            // No es una casilla vacia
-            if (game->tablero[i][j] != 0) {
-                int filaObjetivo = i;
+    switch (direccion)
+    {
+    case 'u':
+        for (int i=0; i < game->tamanoTablero; i++) {
 
-                while ((filaObjetivo > 0) && (game->tablero[filaObjetivo-1][j] == 0)) {
-                    game->tablero[filaObjetivo-1][j] = game->tablero[filaObjetivo][j];
-                    game->tablero[filaObjetivo][j] = 0;
-                    filaObjetivo--;
-                    casillasMovidas++;
+            for (int j=0; j < game->tamanoTablero; j++) {
+                
+                // No es una casilla vacia
+                if (game->tablero[i][j] != 0) {
+                    int filaObjetivo = i;
+
+                    while ((filaObjetivo > 0) && (game->tablero[filaObjetivo-1][j] == 0)) {
+                        game->tablero[filaObjetivo-1][j] = game->tablero[filaObjetivo][j];
+                        game->tablero[filaObjetivo][j] = 0;
+                        filaObjetivo--;
+                        casillasMovidas++;
+                    }
                 }
             }
         }
+        break;
+
+    case 'd':
+        // j < game->tamanoTablero-1: para evitar salir del rango pues se commpara con el siguiente
+        for (int i=0; i < game->tamanoTablero-1; i++) {
+
+            for (int j=0; j < game->tamanoTablero; j++) {
+                
+                // No es una casilla vacia
+                if (game->tablero[i][j] != 0) {
+                    int filaObjetivo = i;
+
+                    while ((filaObjetivo >= 0) && (game->tablero[filaObjetivo+1][j] == 0)) {
+                        game->tablero[filaObjetivo+1][j] = game->tablero[filaObjetivo][j];
+                        game->tablero[filaObjetivo][j] = 0;
+                        filaObjetivo--;
+                        casillasMovidas++;
+                    }
+                }
+            }
+        }
+        break;
+
+    case 'l':
+        for (int i=0; i < game->tamanoTablero; i++) {
+
+            for (int j=0; j < game->tamanoTablero; j++) {
+                
+                // No es una casilla vacia
+                if (game->tablero[i][j] != 0) {
+                    int columnaObjetivo = j;
+
+                    while ((columnaObjetivo > 0) && (game->tablero[i][columnaObjetivo-1] == 0)) {
+                        game->tablero[i][columnaObjetivo-1] = game->tablero[i][columnaObjetivo];
+                        game->tablero[i][columnaObjetivo] = 0;
+                        columnaObjetivo--;
+                        casillasMovidas++;
+                    }
+                }
+            }
+        }
+        break;
+
+    case 'r':
+        for (int i=0; i < game->tamanoTablero; i++) {
+
+            for (int j=0; j < game->tamanoTablero-1; j++) {
+                
+                // No es una casilla vacia
+                if (game->tablero[i][j] != 0) {
+                    int columnaObjetivo = j;
+
+                    while ((columnaObjetivo >= 0) && (game->tablero[i][columnaObjetivo+1] == 0)) {
+                        game->tablero[i][columnaObjetivo+1] = game->tablero[i][columnaObjetivo];
+                        game->tablero[i][columnaObjetivo] = 0;
+                        columnaObjetivo--;
+                        casillasMovidas++;
+                    }
+                }
+            }
+        }
+        break;
+
+    
+    default:
+        printf("Direccion invalida\n");
+        break;
     }
 
-    if (casillasMovidas) {
-        fusionarCasillas(game, direccion);
-        moverCasillas(game, direccion);
-    }
+    
+
+    // if (casillasMovidas) {
+    //     fusionarCasillas(game, direccion);
+    //     moverCasillas(game, direccion);
+    // }
 
 }
 
 
-void fusionarCasillas(Game* game, char* direccion) {
+void fusionarCasillas(Game* game, char direccion) {
     for (int i=0; i < game->tamanoTablero - 1; i++) {
 
         for (int j=0; j < game->tamanoTablero; j++) {
@@ -41,7 +116,7 @@ void fusionarCasillas(Game* game, char* direccion) {
                 game->tablero[i][j] *= 2;
                 game->tablero[i+1][j] = 0;
 
-                // Pendiente
+                actualizarPuntuacion(game, game->tablero[i][j]);
             }
         }
     }
