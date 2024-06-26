c02254920ea502e5d4754919f65f612f12bf593a
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-25 21:55:02 -0600

    Add time for random numbers and example code for testing board

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include "../include/game-logic.h"
+#include "../include/movement.h"
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
+
+    srand(time(NULL));
+
+    char direccion = 'd';
+
     Game game;
     int game_running = 0;
 
@@ -12,7 +20,30 @@ int main() {
         return 1;
     }
      
-    addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+    // addCasillaRandom(&game);
+
+    game.tablero[0][0] = 2;
+    game.tablero[1][0] = 2;
+    game.tablero[2][0] = 2;
+    game.tablero[3][0] = 2;
+
+    printTablero(&game);
+
+    int casillasMovidas = moverCasillas(&game, direccion);
+
+    if (casillasMovidas) {
+        fusionarCasillas(&game, direccion);
+        //printTablero(&game);
+        moverCasillas(&game, direccion);
+    }
 
     printTablero(&game);
 
