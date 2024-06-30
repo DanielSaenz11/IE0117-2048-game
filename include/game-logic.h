7e492e5daefbf045037dc34f047389d7fb695870
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-06-30 15:24:09 -0600

    Update game-logic.h

--- include/game-logic.h
+++ include/game-logic.h
@@ -5,6 +5,7 @@ typedef struct {
     int **tablero;
     int tamanoTablero;
     int puntuacion;
+    int quit; // Agregar un miembro para indicar si el juego debe finalizar
 } Game;
 
 int init_board(Game* game);
@@ -13,5 +14,7 @@ void addCasillaRandom(Game* game);
 int checkPerder(Game* game);
 void actualizarPuntuacion(Game* game, int puntuacion);
 void freeTablero(Game* game);
+void moverCasillas(Game* game, char direccion);
+void fusionarCasillas(Game* game, char direccion);
 
 #endif // GAME_LOGIC_H
