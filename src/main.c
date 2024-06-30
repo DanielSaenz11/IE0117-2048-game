d025964923857b2cd73435aa2bba7780ac8f806c
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-06-30 13:57:45 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,93 +7,124 @@
 #include <time.h>
 #include <SDL2/SDL.h>
 
-#define TICK_INTERVAL 100
-
 int main() {
-    // Inicialización de SDL y creación de ventana
-    initSDL();
+    // Inicialización de SDL y creación de ventana para pedir tamaño del tablero
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Error al inicializar SDL: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
 
-    // Inicialización del juego
-    srand(time(NULL));
-    Game game;
-    int game_running = 0;
+    SDL_Window *ventanaInicial = SDL_CreateWindow("2048 - Tamaño del Tablero",
+                                                  SDL_WINDOWPOS_CENTERED,
+                                                  SDL_WINDOWPOS_CENTERED,
+                                                  400, 200, 0);
 
-    game.tamanoTablero = checkSize(&game);
-    game_running = init_board(&game);
-
-    if (!game_running) {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(ventana);
+    if (!ventanaInicial) {
+        fprintf(stderr, "Error al crear la ventana inicial: %s\n", SDL_GetError());
         SDL_Quit();
         return EXIT_FAILURE;
     }
 
-    // Bucle principal del juego
-    SDL_Event event;
-    Uint32 next_game_tick = SDL_GetTicks();
-    char direccion = ' ';
-    int quit = 0;
+    SDL_Renderer *rendererInicial = SDL_CreateRenderer(ventanaInicial, -1, SDL_RENDERER_ACCELERATED);
+    if (!rendererInicial) {
+        fprintf(stderr, "Error al crear el renderer inicial: %s\n", SDL_GetError());
+        SDL_DestroyWindow(ventanaInicial);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
-    while (!quit && !(checkPerder(&game))) {
-        // Manejar eventos de SDL
+    SDL_bool ventanaAbierta = SDL_TRUE;
+    SDL_Event event;
+    int tamanoTablero = 0;
+
+    while (ventanaAbierta) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = 1;
-            } else if (event.type == SDL_KEYDOWN) {
-                switch (event.key.keysym.sym) {
-                    case SDLK_UP:
-                        direccion = 'u';
-                        break;
-                    case SDLK_DOWN:
-                        direccion = 'd';
-                        break;
-                    case SDLK_LEFT:
-                        direccion = 'l';
-                        break;
-                    case SDLK_RIGHT:
-                        direccion = 'r';
-                        break;
-                    case SDLK_q:
-                        quit = 1;
-                        break;
-                    default:
-                        break;
-                }
+            switch (event.type) {
+                case SDL_QUIT:
+                    ventanaAbierta = SDL_FALSE;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_3:
+                            tamanoTablero = 3;
+                            ventanaAbierta = SDL_FALSE;
+                            break;
+                        case SDLK_4:
+                            tamanoTablero = 4;
+                            ventanaAbierta = SDL_FALSE;
+                            break;
+                        case SDLK_5:
+                            tamanoTablero = 5;
+                            ventanaAbierta = SDL_FALSE;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
 
-        if (direccion != ' ') {
-            // Realizar movimiento y actualizaciones del juego
-            moverCasillas(&game, direccion);
-            fusionarCasillas(&game, direccion);
-            moverCasillas(&game, direccion);
+        SDL_SetRenderDrawColor(rendererInicial, 255, 255, 255, 255);
+        SDL_RenderClear(rendererInicial);
 
-            // Reiniciar la dirección después de cada movimiento
-            direccion = ' ';
+        // Dibujar texto en la ventana inicial
+        // Puedes usar SDL_ttf para manejar fuentes si necesitas texto más complejo
 
-            // Actualizar renderizado del tablero
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Color de fondo blanco
-            SDL_RenderClear(renderer);
-
-            renderizarTablero(&game);  // Llamar a la función para renderizar el tablero
-
-            SDL_RenderPresent(renderer);  // Mostrar el renderizado en pantalla
-        }
-
-        // Esperar para mantener una tasa de fotogramas estable (opcional)
-        Uint32 now = SDL_GetTicks();
-        if (next_game_tick <= now) {
-            next_game_tick = now + TICK_INTERVAL;
-        } else {
-            SDL_Delay(next_game_tick - now);
-        }
+        SDL_RenderPresent(rendererInicial);
     }
 
-    // Liberar recursos y salir
-    freeTablero(&game);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(ventana);
-    SDL_Quit();
+    SDL_DestroyRenderer(rendererInicial);
+    SDL_DestroyWindow(ventanaInicial);
 
-    return EXIT_SUCCESS;
+    // Iniciar el juego con el tamaño seleccionado
+    if (tamanoTablero >= 3 && tamanoTablero <= 5) {
+        // Inicialización del juego con el tamaño seleccionado
+        srand(time(NULL));
+        Game game;
+        int game_running = 0;
+
+        game.tamanoTablero = tamanoTablero;
+        game_running = init_board(&game);
+
+        if (!game_running) {
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
+
+        // Crear ventana y renderer para el juego
+        SDL_Window *ventana = SDL_CreateWindow("2048",
+                                              SDL_WINDOWPOS_CENTERED,
+                                              SDL_WINDOWPOS_CENTERED,
+                                              game.tamanoTablero * TILE_SIZE,
+                                              game.tamanoTablero * TILE_SIZE, 0);
+
+        if (!ventana) {
+            fprintf(stderr, "Error al crear la ventana: %s\n", SDL_GetError());
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
+
+        SDL_Renderer *renderer = SDL_CreateRenderer(ventana, -1, SDL_RENDERER_ACCELERATED);
+        if (!renderer) {
+            fprintf(stderr, "Error al crear el renderer: %s\n", SDL_GetError());
+            SDL_DestroyWindow(ventana);
+            SDL_Quit();
+            return EXIT_FAILURE;
+        }
+
+        // Bucle principal del juego (adaptado de tu código existente)
+        // Aquí continuarías con el bucle principal y renderizado del juego
+
+        // Liberar recursos y salir al cerrar el juego
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(ventana);
+        SDL_Quit();
+        return EXIT_SUCCESS;
+    } else {
+        // El usuario cerró la ventana inicial sin seleccionar tamaño válido
+        SDL_Quit();
+        return EXIT_SUCCESS;
+    }
 }
