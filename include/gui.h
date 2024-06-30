d3c796860dc2d8dcfab233818bffe2675252c025
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-06-30 02:55:13 -0600

    Update gui.h

--- include/gui.h
+++ include/gui.h
@@ -1,9 +1,15 @@
 // gui.h
+
 #ifndef GUI_H
 #define GUI_H
 
-#include "game-logic.h"
+#include <SDL2/SDL.h>
 
+// Declaración de variables globales como externas
+extern SDL_Window *ventana;
+extern SDL_Renderer *renderer;
+
+// Declaración de funciones para la interfaz gráfica
 void initSDL();
 void renderizarTablero(Game *game);
 void manejarEventos(Game *game);
