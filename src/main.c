b23e4c18c9f2fa0c835bc069a2d36059feec1d25
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-06-30 02:52:51 -0600

    Modificacion del main.c para que implemente la interfaz de gui.c

--- src/main.c
+++ src/main.c
@@ -1,45 +1,99 @@
 // main.c
+
 #include "../include/game-logic.h"
 #include "../include/movement.h"
 #include "../include/gui.h"
 #include <stdlib.h>
 #include <time.h>
+#include <SDL2/SDL.h>
+
+#define TICK_INTERVAL 100
 
 int main() {
+    // Inicialización de SDL y creación de ventana
+    initSDL();
 
+    // Inicialización del juego
     srand(time(NULL));
-
-    char direccion;
-
     Game game;
     int game_running = 0;
 
-    
     game.tamanoTablero = checkSize(&game);
-
     game_running = init_board(&game);
-    
 
     if (!game_running) {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(ventana);
+        SDL_Quit();
         return EXIT_FAILURE;
     }
 
-    do {
-        addCasillaRandom(&game);
+    // Bucle principal del juego
+    SDL_Event event;
+    Uint32 next_game_tick = SDL_GetTicks();
+    char direccion = ' ';
+    int quit = 0;
 
-        printTablero(&game);
+    while (!quit && !(checkPerder(&game))) {
+        // Manejar eventos de SDL
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = 1;
+            } else if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_UP:
+                        direccion = 'u';
+                        break;
+                    case SDLK_DOWN:
+                        direccion = 'd';
+                        break;
+                    case SDLK_LEFT:
+                        direccion = 'l';
+                        break;
+                    case SDLK_RIGHT:
+                        direccion = 'r';
+                        break;
+                    case SDLK_q:
+                        quit = 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
 
-        direccion = checkTecla();
+        if (direccion != ' ') {
+            // Realizar movimiento y actualizaciones del juego
+            moverCasillas(&game, direccion);
+            fusionarCasillas(&game, direccion);
+            moverCasillas(&game, direccion);
 
-        if (direccion == 'q') break;
+            // Reiniciar la dirección después de cada movimiento
+            direccion = ' ';
 
-        moverCasillas(&game, direccion);
-        fusionarCasillas(&game, direccion);
-        moverCasillas(&game, direccion);
+            // Actualizar renderizado del tablero
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Color de fondo blanco
+            SDL_RenderClear(renderer);
 
-    } while ((direccion != 'q') && !(checkPerder(&game)));
+            renderizarTablero(&game);  // Llamar a la función para renderizar el tablero
 
+            SDL_RenderPresent(renderer);  // Mostrar el renderizado en pantalla
+        }
+
+        // Esperar para mantener una tasa de fotogramas estable (opcional)
+        Uint32 now = SDL_GetTicks();
+        if (next_game_tick <= now) {
+            next_game_tick = now + TICK_INTERVAL;
+        } else {
+            SDL_Delay(next_game_tick - now);
+        }
+    }
+
+    // Liberar recursos y salir
     freeTablero(&game);
-    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(ventana);
+    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
