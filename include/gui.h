ffa1697b3464232fc9ac1566cbf3f100eab66f26
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-06-30 15:37:30 -0600

    Update gui.h

--- include/gui.h
+++ include/gui.h
@@ -1,8 +1,10 @@
 #ifndef GUI_H
 #define GUI_H
 
-#include "window.h"
-#include "game-logic.h"
+#include <stdbool.h>  // Incluir para usar el tipo de dato bool y las constantes true/false
+#include "../include/gui.h"
+#include "../include/window.h"
+#include <SDL2/SDL.h>
 
 int initSDLAndWindow(Window* window);
 void renderizarTablero(Game* game, SDL_Renderer* renderer);
