c32b351f86d67e7f9956ccac6736d25d01a27d1b
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-06-30 15:14:04 -0600

    Update game-logic.h

--- include/game-logic.h
+++ include/game-logic.h
@@ -1,31 +1,20 @@
-#ifndef GAME_LOGIC_H
+// game-logic.h
 
+#ifndef GAME_LOGIC_H
 #define GAME_LOGIC_H
 
-typedef struct Game {
-    int **tablero; // Doble puntero para tener un array 2D
-    int puntuacion;
+#include "gui.h" // Incluir gui.h para la definición de Game si es necesario
+
+typedef struct {
+    // Definición de campos necesarios para la lógica del juego
     int tamanoTablero;
+    // Otros campos según sea necesario
 } Game;
 
-int checkSize();
-
-char checkTecla();
-
 int init_board(Game* game);
-
-void printTablero(Game* game); // Terminal debugging
-
-void addCasillaRandom(Game* game);
-
 int checkPerder(Game* game);
-
-void actualizarPuntuacion(Game* game, int puntuacion);
-
-void freeTablero(Game* game);
-
-// Declaraciones adicionales para las funciones en game-logic.c
 int moverCasillas(Game* game, char direccion);
 void fusionarCasillas(Game* game, char direccion);
+void freeTablero(Game* game);
 
 #endif /* GAME_LOGIC_H */
