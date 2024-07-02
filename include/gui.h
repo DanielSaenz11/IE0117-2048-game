50d5a179ec37c5f0ad5ae9d9fd46c05236f967a0
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-07-02 01:09:39 -0600

    Update gui.h

--- include/gui.h
+++ include/gui.h
@@ -2,7 +2,6 @@
 #define GUI_H
 
 #include <SDL2/SDL.h>
-#include "game.h"  // Incluye el archivo donde está definida la estructura Game
 
 // Función para renderizar el tablero
 void render_board(Game *game, SDL_Renderer *renderer);
