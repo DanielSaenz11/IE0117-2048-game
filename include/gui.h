ad6e3d3dc739e7b28112108981b6433874dae8d3
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-07-03 03:48:37 -0600

    Update gui.h

--- include/gui.h
+++ include/gui.h
@@ -8,7 +8,6 @@
 // Declaración de funciones específicas de la GUI
 void render_board(Game *game, SDL_Renderer *renderer);
 
-void show_board_size_selection(SDL_Renderer *renderer, int *selected_size) 
 
 #endif /* GUI_H */
 
