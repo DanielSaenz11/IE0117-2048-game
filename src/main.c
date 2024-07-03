2856f5e2577ae0d8e1c4cf8b18fb2b362ef94e10
ValeRomanA <147361054+ValeRomanA@users.noreply.github.com>
2024-07-03 00:02:32 -0600

    Update main.c modificacion de main para la ejecucion del juego

--- src/main.c
+++ src/main.c
@@ -1,45 +1,77 @@
-// main.c
 #include "../include/game-logic.h"
 #include "../include/movement.h"
 #include "../include/gui.h"
 #include <stdlib.h>
 #include <time.h>
+#include <SDL2/SDL.h>
 
 int main() {
-
     srand(time(NULL));
 
-    char direccion;
-
     Game game;
-    int game_running = 0;
+    printf("Enter board size: ");
+    scanf("%d", &game.tamanoTablero);
 
-    
-    game.tamanoTablero = checkSize(&game);
+    if (!init_board(&game)) {  // Validación adicional para init_board
+        printf("Failed to initialize the board.\n");
+        return 1;
+    }
+    addCasillaRandom(&game);  // Añadir las primeras dos casillas aleatorias
+    addCasillaRandom(&game);
 
-    game_running = init_board(&game);
-    
-
-    if (!game_running) {
-        return EXIT_FAILURE;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
     }
 
-    do {
-        addCasillaRandom(&game);
+    SDL_Window *window = SDL_CreateWindow("2048", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, game.tamanoTablero * 100, game.tamanoTablero * 100, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
-        printTablero(&game);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
-        direccion = checkTecla();
+    int quit = 0;
+    SDL_Event e;
+    while (!quit && !checkPerder(&game)) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            } else if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_UP:
+                        moverCasillas(&game, 'w');
+                        addCasillaRandom(&game);
+                        break;
+                    case SDLK_DOWN:
+                        moverCasillas(&game, 's');
+                        addCasillaRandom(&game);
+                        break;
+                    case SDLK_LEFT:
+                        moverCasillas(&game, 'a');
+                        addCasillaRandom(&game);
+                        break;
+                    case SDLK_RIGHT:
+                        moverCasillas(&game, 'd');
+                        addCasillaRandom(&game);
+                        break;
+                }
+            }
+        }
+        render_board(&game, renderer);
+        SDL_Delay(100);
+    }
 
-        if (direccion == 'q') break;
-
-        moverCasillas(&game, direccion);
-        fusionarCasillas(&game, direccion);
-        moverCasillas(&game, direccion);
-
-    } while ((direccion != 'q') && !(checkPerder(&game)));
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     freeTablero(&game);
-    
-    return EXIT_SUCCESS;
+
+    return 0;
 }
