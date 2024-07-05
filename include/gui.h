a20d2b054cadb2fb372e4a1a7695d248efa2df5d
DanielSaenz11 <dsaenzobando@gmail.com>
2024-07-05 13:30:22 -0600

    Documentation for render_tablero func

--- include/gui.h
+++ include/gui.h
@@ -1,12 +1,19 @@
 #ifndef GUI_H
 #define GUI_H
 
+// Inclusion de librerias de SDL
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h> // Ejemplo de inclusión adicional
+#include <SDL2/SDL_ttf.h>
 #include "game-logic.h"
 
-// Declaración de funciones específicas de la GUI
-void render_board(Game *game, SDL_Renderer *renderer);
+/*
+ * render_tablero() se encarga de renderizar el tablero para mostrar en la ventana
+ * los movimientos y acciones del usuario
+ * 
+ * @param1 Game* game: Puntero al struct que almacena las propiedades del juego
+ * @param2 SDL_Renderer* renderer: Puntero al renderer propio de SDL
+ */
+void render_tablero(Game *game, SDL_Renderer *renderer);
 
 
 #endif /* GUI_H */
