47184286fd3cbd771fff9a65ca88510b03e4c70f
DanielSaenz11 <dsaenzobando@gmail.com>
2024-07-05 23:56:47 -0600

    Fixed format

--- src/gui.c
+++ src/gui.c
@@ -24,6 +24,7 @@ int render_tablero(Game* game, SDL_Renderer* renderer) {
 
     // Cargar la fuente para el tablero y la puntuacion
     TTF_Font *font = TTF_OpenFont("./NightPumpkind-1GpGv.ttf", 24);  // Ruta y tamano de la fuente
+
     if (font == NULL) {
         fprintf(stderr, "Error al cargar la fuente del programa: %s\n", TTF_GetError());
         return NULL_POINTER;
@@ -34,72 +35,92 @@ int render_tablero(Game* game, SDL_Renderer* renderer) {
     // Renderizar la puntuacion
     char puntuacion[20];
     sprintf(puntuacion, "Puntuacion: %d", game->puntuacion);
+
     SDL_Surface *superficie_puntuacion = TTF_RenderText_Solid(font, puntuacion, textColor);
+
     if (superficie_puntuacion == NULL) {
         fprintf(stderr, "Error al renderizar superficie el texto de la puntuación. SDL_ttf Error: %s\n", TTF_GetError());
         TTF_CloseFont(font);
         return NULL_POINTER;
     }
+
     else {
         SDL_Texture *textura_puntuacion = SDL_CreateTextureFromSurface(renderer, superficie_puntuacion);
+        
         if (textura_puntuacion == NULL) {
+
             fprintf(stderr, "Error al crear textura de superficie de la puntuacion. SDL Error: %s\n", SDL_GetError());
             SDL_FreeSurface(superficie_puntuacion);
             TTF_CloseFont(font);
+
             return NULL_POINTER;
 
         } else {
             SDL_Rect puntRectangulo;
+
             puntRectangulo.x = 10;  // Posición en X de la puntuación
             puntRectangulo.y = 10;  // Posición en Y de la puntuación
+
             puntRectangulo.w = superficie_puntuacion->w;
             puntRectangulo.h = superficie_puntuacion->h;
+
             SDL_RenderCopy(renderer, textura_puntuacion, NULL, &puntRectangulo);
             SDL_DestroyTexture(textura_puntuacion);
         }
         SDL_FreeSurface(superficie_puntuacion);
     }
 
-    // Iterar sobre el tablero para renderizar cada celda
+    // Iteracion tablero para renderizr celda
     for (int i = 0; i < game->tamanoTablero; i++) {
+
         for (int j = 0; j < game->tamanoTablero; j++) {
+
             SDL_Rect celda;
+
             celda.x = j * tamano_celda;
-            celda.y = i * tamano_celda + margen_superior;  // Desplazar el tablero hacia abajo para hacer espacio para la puntuación
+            celda.y = i * tamano_celda + margen_superior;  // Desplazar el tablero hacia abajo para hacer espacio para la puntuacion
+            
             celda.w = tamano_celda - 5;
             celda.h = tamano_celda - 5;
 
-            // Configurar color y renderizar rectángulo de la celda
+            // Configurar color y renderizar rectangulo de la celda
             if (game->tablero[i][j] == 0) {
-                SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);  // Color para celdas vacías
+                SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);  // Color para celdas vacias
+            
             } else {
                 SDL_SetRenderDrawColor(renderer, 100 + game->tablero[i][j] * 10, 100 + game->tablero[i][j] * 5, 100 + game->tablero[i][j] * 2, 255);  // Color dependiendo del valor en la celda
             }
+
             SDL_RenderFillRect(renderer, &celda);
 
-            // Renderizar texto si la celda no está vacía
+            // Renderizar texto si la celda no esta vacia
             if (game->tablero[i][j] != 0) {
                 char valorCasilla[5];
+
                 sprintf(valorCasilla, "%d", game->tablero[i][j]); // Agregar valor de la casilla
 
                 // Renderizar texto
                 SDL_Surface *superficie_texto = TTF_RenderText_Solid(font, valorCasilla, textColor);
+
                 if (superficie_texto == NULL) {
                     fprintf(stderr, "Error al renderizar superficie el texto. SDL_ttf Error: %s\n", TTF_GetError());
                     return NULL_POINTER;
                 }
 
                 SDL_Texture *textura_texto = SDL_CreateTextureFromSurface(renderer, superficie_texto);
+
                 if (textura_texto == NULL) {
                     fprintf(stderr, "Error al crear textura de superficie. SDL Error: %s\n", SDL_GetError());
                     SDL_FreeSurface(superficie_texto);
                     return NULL_POINTER;
                 }
 
-                // Configurar posición del texto dentro de la celda
+                // Configurar posicion del texto dentro de la celda
                 SDL_Rect textRect;
+
                 textRect.x = celda.x + (tamano_celda - superficie_texto->w) / 2;
                 textRect.y = celda.y + (tamano_celda - superficie_texto->h) / 2;
+                
                 textRect.w = superficie_texto->w;
                 textRect.h = superficie_texto->h;
 
@@ -112,7 +133,7 @@ int render_tablero(Game* game, SDL_Renderer* renderer) {
             }
         }
     }
-    // Cerrar la fuente después de su uso
+    // Cerrar la fuente despues de su uso
     TTF_CloseFont(font);
 
     // Actualizar la pantalla
